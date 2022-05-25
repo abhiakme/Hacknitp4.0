3a8ae0ccc3770feba95f8ffc7fb4bac9db71b6b7
Abhishek kumar <90022163+abhiakme@users.noreply.github.com>
2022-05-25 13:08:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ function fixNav() {
         nav.classList.remove('active')
     }
 }
+function showNotification(){
+    const notification =new Notification("new massage from abhi",{
+        body:"hey,friends how r u"
+        )};
+     }
+        
+console.log(Notification.permission):
+
+ if(Notification.permission === "granted"){
+     alert("we have permission");
+ }else if(Notification.permission !== "denied"){
+     Notification.requestPermission().then(permission =>{
+         console.log(permission);
+     }):
+  }
 
